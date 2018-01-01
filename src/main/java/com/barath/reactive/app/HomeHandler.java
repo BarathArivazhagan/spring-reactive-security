1ecb85fc91ea6aba73a28e8fad01cbfaca2529b9
Barath <barathmacdec91@gmail.com>
2018-01-01 22:06:45 +0530

    adding principal logic

--- src/main/java/com/barath/reactive/app/HomeHandler.java
+++ src/main/java/com/barath/reactive/app/HomeHandler.java
@@ -8,13 +8,16 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.security.Principal;
+
 @Component
 public class HomeHandler {
 
 
     public Mono<ServerResponse> home(ServerRequest request){
 
-        return ServerResponse.ok().body(BodyInserters.fromObject("hello"));
+       Mono<String> userName=request.principal().map(Principal::getName).map( name -> "hello "+name);
+       return ServerResponse.ok().body(userName,String.class);
     }
 
 }
